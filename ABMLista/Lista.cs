359fe01c8576223e32ad94980c55f34e0e37729a
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2019-09-09 23:29:26 -0300

    Initial commit

---
+++ ABMLista/Lista.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABMLista.Clases
+{
+    public class Lista
+    {
+        #region PROPIEDADES
+        private string[] lista = new string[1];
+
+        private int ProximaPosicion = 0;
+        #endregion
+
+        #region CONSTRUCTOR
+        #endregion
+
+        #region METODOS
+        public bool Agregar(string aTexto)
+        {
+            bool Resp = false;
+            try
+            {
+                if (ProximaPosicion == lista.Length)
+                {
+                    this.AgregaRegistro(1);
+                }
+
+                lista[ProximaPosicion] = aTexto;
+                ProximaPosicion++;
+                Resp = true;
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+
+            return Resp;
+        }
+
+        public string MostrarLista()
+        {
+
+            string Respuesta = "";
+            if (ProximaPosicion > 0)
+            {
+                Respuesta = lista[0];
+                for (int i = 1; i < ProximaPosicion; i++)
+                {
+                    Respuesta = Respuesta + "\r\n" + lista[i];
+                }
+            }
+            return Respuesta;
+        }
+
+        private void AgregaRegistro(int incremento)
+        {
+            string[] Temp = new string[lista.Length + incremento];
+            lista = this.Copiar(lista, Temp);
+        }
+
+        private string[] Copiar(string[] Origen, string[] Destino)
+        {
+            for (int i = 0; i < ProximaPosicion; i++)
+            {
+                Destino[i] = Origen[i];
+            }
+            return Destino;
+        }
+
+        /// <summary>
+        /// Devuelve la posicion del texto (entero) dentro del arregla, <br/>
+        /// la cuenta inicia en 0 (cero).
+        /// Devuelve -1 cuando no encuentra Que en la lista.
+        /// </summary>
+        /// <param name="Que">texto completo a buscar en la lista</param>
+        public int BuscarPosicion(string Que)
+        {
+            int Resp = -1;
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                if (lista[i].CompareTo(Que) == 0)
+                {
+                    Resp = i;
+                    break;
+                }
+            }
+
+            return Resp;
+        }
+
+        public string Borrar(string Que)
+        {
+            string Resp = "";
+            int Pos = this.BuscarPosicion(Que);
+            if (Pos == -1)
+            {
+                Resp = Que + " no ha sido encontrado en la lista.";
+            }
+            else
+            {
+                for (int i = Pos; i < ProximaPosicion - 1; i++)
+                {
+                    this.lista[i] = this.lista[i + 1];
+                }
+                this.lista[ProximaPosicion - 1] = null;
+                ProximaPosicion = ProximaPosicion - 1;
+                AgregaRegistro(-1);
+            }
+
+            return Resp;
+        }
+
+        public string Ordenar()
+        {
+            string[] listaOrden = new string[lista.Length];
+            string Respuesta = "";
+
+            Copiar(lista, listaOrden);
+
+            if (listaOrden.Length > 1)
+            {
+                for (int i = 0; i < listaOrden.Length - 1; i++)
+                {
+                    for (int j = 0; j < listaOrden.Length - 1; j++)
+                    {
+                        if ((listaOrden[j].CompareTo(listaOrden[j + 1])) > 0)
+                        {
+                            string temp2 = "";
+
+                            temp2 = listaOrden[j];
+                            listaOrden[j] = listaOrden[j + 1];
+                            listaOrden[j + 1] = temp2;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < listaOrden.Length; i++)
+            {
+                Respuesta = Respuesta + listaOrden[i] + "\r\n";
+            }
+            return Respuesta;
+        }
+
+        #endregion
+        //public string MostrarOrdenada()
+        //{
+        //    string[] listaOrden;
+        //    listaOrden = lista.OrderBy(x => x).ToArray();
+
+        //    string Respuesta = "";
+        //    if (ProximaPosicion >= 0)
+        //    {
+        //        Respuesta = listaOrden[0];
+        //        for (int i = 1; i < ProximaPosicion; i++)
+        //        {
+        //            Respuesta = Respuesta + "\r\n" + listaOrden[i];
+        //        }
+        //    }
+        //    return Respuesta;
+        //}
+    }
+
+}
---
+++ ABMLista/frm.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace ABMLista
+{
+    partial class frm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btAgregar = new System.Windows.Forms.Button();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.lbl = new System.Windows.Forms.Label();
+            this.btBuscar = new System.Windows.Forms.Button();
+            this.btBorrar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.btOrdenar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btAgregar
+            // 
+            this.btAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAgregar.Location = new System.Drawing.Point(641, 57);
+            this.btAgregar.Margin = new System.Windows.Forms.Padding(4);
+            this.btAgregar.Name = "btAgregar";
+            this.btAgregar.Size = new System.Drawing.Size(100, 28);
+            this.btAgregar.TabIndex = 1;
+            this.btAgregar.Text = "Agregar";
+            this.btAgregar.UseVisualStyleBackColor = true;
+            this.btAgregar.Click += new System.EventHandler(this.BtAgregar_Click);
+            // 
+            // txt
+            // 
+            this.txt.BackColor = System.Drawing.Color.DimGray;
+            this.txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt.ForeColor = System.Drawing.SystemColors.Info;
+            this.txt.Location = new System.Drawing.Point(16, 15);
+            this.txt.Margin = new System.Windows.Forms.Padding(4);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(725, 30);
+            this.txt.TabIndex = 0;
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(40, 98);
+            this.lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(15, 20);
+            this.lbl.TabIndex = 4;
+            this.lbl.Text = " ";
+            // 
+            // btBuscar
+            // 
+            this.btBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btBuscar.Location = new System.Drawing.Point(641, 98);
+            this.btBuscar.Margin = new System.Windows.Forms.Padding(4);
+            this.btBuscar.Name = "btBuscar";
+            this.btBuscar.Size = new System.Drawing.Size(100, 28);
+            this.btBuscar.TabIndex = 2;
+            this.btBuscar.Text = "Buscar";
+            this.btBuscar.UseVisualStyleBackColor = true;
+            this.btBuscar.Click += new System.EventHandler(this.BtBuscar_Click);
+            // 
+            // btBorrar
+            // 
+            this.btBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btBorrar.Location = new System.Drawing.Point(641, 145);
+            this.btBorrar.Margin = new System.Windows.Forms.Padding(4);
+            this.btBorrar.Name = "btBorrar";
+            this.btBorrar.Size = new System.Drawing.Size(100, 28);
+            this.btBorrar.TabIndex = 3;
+            this.btBorrar.Text = "Borrar";
+            this.btBorrar.UseVisualStyleBackColor = true;
+            this.btBorrar.Click += new System.EventHandler(this.BtBorrar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(13, 414);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(619, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(641, 414);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(380, 98);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(15, 20);
+            this.lbl2.TabIndex = 7;
+            this.lbl2.Text = " ";
+            // 
+            // btOrdenar
+            // 
+            this.btOrdenar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btOrdenar.Location = new System.Drawing.Point(641, 194);
+            this.btOrdenar.Margin = new System.Windows.Forms.Padding(4);
+            this.btOrdenar.Name = "btOrdenar";
+            this.btOrdenar.Size = new System.Drawing.Size(100, 28);
+            this.btOrdenar.TabIndex = 8;
+            this.btOrdenar.Text = "Ordenar";
+            this.btOrdenar.UseVisualStyleBackColor = true;
+            this.btOrdenar.Click += new System.EventHandler(this.BtOrdenar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(379, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Por orden alfabético";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(215, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Por orden de entrada";
+            // 
+            // btnListar
+            // 
+            this.btnListar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListar.Location = new System.Drawing.Point(641, 242);
+            this.btnListar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(100, 28);
+            this.btnListar.TabIndex = 11;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.BtnListar_Click);
+            // 
+            // frm
+            // 
+            this.AcceptButton = this.btAgregar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(756, 554);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btOrdenar);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btBorrar);
+            this.Controls.Add(this.btBuscar);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.btAgregar);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Manejo de lista";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btAgregar;
+        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Button btBuscar;
+        private System.Windows.Forms.Button btBorrar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Button btOrdenar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnListar;
+    }
+}
+
